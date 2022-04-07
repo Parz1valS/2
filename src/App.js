d56094e2ff2312f0e481d1a1ae15abfe59714105
Danila <danila.shulerman123456789@yandex.ru>
2022-04-07 11:57:15 +0300

    Таблица с fetch

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    const tab = (
+        <table>
+            <tbody>
+            <tr>
+                <th>№ зачетки</th>
+                <th>ФИО</th>
+                <th>№ группы</th>
+                <th>Кафедра</th>
+                <th>Факультет</th>
+            </tr>
+            </tbody>
+        </table>
+    );
+     fetch('../src/GetJson.php')
+         .then(response => response.json())
+         .then(data => console.log(data));
+    return (
+        <div>
+            {tab}
+        </div>
+    );
 }
 
 export default App;
