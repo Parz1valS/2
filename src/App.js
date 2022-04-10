8eff7b6b2da5df1f9a6ee0ffe72fed8e54ef7437
Danila <danila.shulerman123456789@yandex.ru>
2022-04-10 21:17:53 +0300

    Сработало json.php вывелся в консоли

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import axios from "axios";
 
 function App() {
 
@@ -15,9 +16,42 @@ function App() {
             </tbody>
         </table>
     );
-     fetch('../src/GetJson.php')
+     fetch('./src/GetJson.php')
          .then(response => response.json())
          .then(data => console.log(data));
+   // axios({
+   //     method: 'POST',
+   //     url: './src/GetJson.php',
+   //     data: [
+   //              <tr>
+   //             <td>'RECORD_BOOK'</td>
+   //             <td>'FIO'</td>
+   //             <td>'GROUP_NUMBER'</td>
+   //             <td>'CHAIR'</td>
+   //             <td>'FACULTY'</td>
+   //         </tr>
+   //          ]
+   // })
+   //  axios({
+   //      method: 'get',
+   //      headers: {'Content-Type': 'application/x-www-form-urlencoded'},
+   //      url: './src/GetJson.php',
+   //      data: [
+   //          <tr>
+   //              <td>'RECORD_BOOK'</td>
+   //              <td>'FIO'</td>
+   //              <td>'GROUP_NUMBER'</td>
+   //              <td>'CHAIR'</td>
+   //              <td>'FACULTY'</td>
+   //          </tr>
+   //      ]
+            // .then(function (response) {
+            //     console.log(response);
+            // })
+            // .catch(function (error) {
+            //     console.log(error);
+            // })
+    //})
     return (
         <div>
             {tab}
